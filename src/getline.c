2c972b24bfde06dfe594e9ca04f9eda4dda15732
Nextime <evan.lebihan@ecole-89.com>
2023-05-11 16:00:35 +0200

    update

---
+++ src/getline.c
@@ -0,0 +1,39 @@
+/*
+ * E89 Pedagogical & Technical Lab
+ * project:     Getline
+ * created on:  2023-05-04 - 15:44 +0200
+ * 1st author:  evan.lebihan - evan.lebihan
+ * description: Getline.c
+ */
+
+#include "stu.h"
+
+char *stu_getline(int fd)
+{
+    static char *carry = NULL;
+    static int fdsave = -1;
+    char *to_return = NULL;
+    char *to_free;
+
+    if (fd == -1) {
+        if (carry == NULL) {
+            free(carry);
+        }
+            return (NULL);
+    }
+    if (fd != fdsave) {
+        free(carry);
+        carry = NULL;
+        fdsave = fd;
+    }
+    if (carry == NULL) {
+        to_return = loop_read_until(fd, '\n');
+    } else {
+        to_free = to_return;
+        to_return = carry;
+        free(to_free);
+        to_return = strdupcat(carry, loop_read_until(fd, '\n'));
+    }
+    carry = stu_strdup(split_inplace(to_return, '\n'));
+    return (to_return);
+}
---
+++ src/loop_read_until.c
@@ -0,0 +1,37 @@
+/*
+ * E89 Pedagogical & Technical Lab
+ * project:     loop read until
+ * created on:  2023-02-21 - 10:45 +0100
+ * 1st author:  evan.lebihan - evan.lebihan
+ * description: loop_read_until.c
+ */
+
+#include "stu.h"
+
+char *loop_read_until(int fd, char token)
+{
+    char *to_free;
+    char *to_return;
+    int size_read;
+    int i;
+
+    to_free = malloc(sizeof(char) * 17);
+    size_read = read(fd, to_free, 16);
+    to_free[size_read] = '\0';
+    to_return = strdup(to_free);
+    while (size_read > 0) {
+        i = 0;
+        while (i < 16 && to_free[i] != '\0' && to_free[i] != token) {
+            i += 1;
+        }
+        if (to_free[i] == token && size_read > 0) {
+            free(to_free);
+            return (to_return);
+        }
+        size_read = read(fd, to_free, 16);
+        to_free[size_read] = '\0';
+        to_return = strdupcat(to_return, to_free);
+    }
+    free(to_free);
+    return (to_return);
+}
